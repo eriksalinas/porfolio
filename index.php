9ab5b7198206f8917fcb672a6621d77c35016239
eriksalinas <erik61869@gmail.com>
2022-04-06 16:11:30 -0300

    cambios

--- index.php
+++ index.php
@@ -11,21 +11,20 @@
     <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
     <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/estilos.css">
+
 </head>
 
 <body id="inicio">
     <header class="container">
         <nav class="navbar navbar-expand-md  mb-4">
             <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa-solid fa-bars"></i>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
                         <li class="nav-item">
-                            <a class="nav-link py-1 px-3 px-sm-4 active " aria-current="page"
-                                href="index.html">Inicio</a>
+                            <a class="nav-link py-1 px-3 px-sm-4 active " aria-current="page" href="index.html">Inicio</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link py-1 px-3 px-sm-4 " href="sobre-mi.php">Sobre mí</a>
@@ -46,13 +45,19 @@
     </header>
     <main class="container">
         <div class="row">
-            <div class="col-12 col-sm-4 " >
-               
-                <img src="imagen/drive-download-20220317T011215Z-001/cohete.svg"  >
-             
+            <div class="col-12 mt-4  text-center ">
+            
+                <img src="imagen/drive-download-20220317T011215Z-001/cohete.svg" class="choete">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 col-sm-6 my-1 text-center offset-sm-3 mb-3">
+                <p class="p-1">Bienvenid@ a mi sitio web sobre docencia en sistemas</p>
             </div>
         </div>
     </main>
+
     <footer class="container mt-5">
         <!-- Se utiliza mt-5 para achicar o agrandar el contenido -->
         <div class="row">
